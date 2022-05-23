5aeb71b73249e4d53400e0d32e1a6c68d5c6d507
Alan Tanaka <alan@teste.com>
2022-05-22 23:24:02 -0300

    Views and templates defined

--- logs/urls.py
+++ logs/urls.py
@@ -4,8 +4,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "logs"
 
 urlpatterns = [
-    path("", views.Index.as_view(), name="index"),
+    path("", views.index, name="index"),
+    path("topics", views.topics, name="topics"),
+    path("topics/<int:pk>", views.topic, name="topic"),
     # path("", index.as_view(), name="index"),
 ]
--- logs/views.py
+++ logs/views.py
@@ -1,5 +1,22 @@
-from django.views.generic.base import TemplateView
+from django.shortcuts import render
+from .models import Topic
 
 
-class Index(TemplateView):
-    template_name = "index.html"
+def index(request):
+    """A página inicial de Learning Log"""
+    return render(request, "logs/index.html")
+
+
+def topics(request):
+    """Mostra todos os assuntos."""
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics": topics}
+    return render(request, "logs/topics.html", context)
+
+
+def topic(request, pk):
+    """Mostra um único assunto e todas as suas entradas."""
+    topic = Topic.objects.get(id=pk)
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entries}
+    return render(request, "logs/topic.html", context)
